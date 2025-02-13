18bf3dfe1525ce3b5d9671ecb053b4ee659c4af0
Romit Patil <romit5574@gmail.com>
2025-02-13 18:44:46 +0530

    Added Game End Dashboard with Restart Button

---
+++ Assets/Bowling_WebGL/Scripts/GameEndManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameEndManager : MonoBehaviour
+{
+    public TMP_Text finalScoreText;
+    public Button playAgainButton;
+
+    private void Start()
+    {
+        finalScoreText.text = $"Total Score: {ScoreManager.Instance.GetTotalScore()}";
+        playAgainButton.onClick.AddListener(RestartGame);
+    }
+
+    private void RestartGame(){
+        SceneManager.LoadScene("MainScene"); 
+    }
+}
